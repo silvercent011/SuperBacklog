53bc53f9d44ad3c531f4a35474bb97591979799a
Sidney Alex <38598808+silvercent011@users.noreply.github.com>
2022-10-23 23:42:15 -0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./components/**/*.{vue,js}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./**/*.vue",
+    "./*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
